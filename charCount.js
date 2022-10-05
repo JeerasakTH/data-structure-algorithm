ba36d3a940ebdcf22694099e4579ed4c3ddc144d
Jeerasak Todsungnern <78092831+JeerasakTH@users.noreply.github.com>
2022-10-05 16:39:49 +0700

    initial commit

---
+++ charCount.js
@@ -0,0 +1,22 @@
+const charCount = (str) => {
+  // make object to return at end
+  let result = {};
+  // loop over string, for each character...
+  //   for (let i = 0; i < str.length; i++) {
+  for (let char of str) {
+    char = char.toLowerCase();
+    // if the char is a number/letter AND is a key in object, add one to count
+    if (result[char] > 0) {
+      result[char]++;
+    }
+    // if the char is a number/letter AND not in object, add it to object and set value to 1
+    else {
+      result[char] = 1;
+    }
+  }
+  // if character is something else (space, period, etc.) don't do anything
+  // return object at end
+  return result;
+};
+
+console.log(charCount("test"));
