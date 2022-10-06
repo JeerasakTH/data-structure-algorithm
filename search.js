aeb793bffb8f0842744424bbd180835d05aeb0ef
Jeerasak Todsungnern <78092831+JeerasakTH@users.noreply.github.com>
2022-10-06 16:24:21 +0700

    day 2

---
+++ search.js
@@ -0,0 +1,16 @@
+const search = (arr, val) => {
+  let min = 0;
+  let max = arr.length - 1;
+
+  while (min <= max) {
+    let middle = Math.floor((min + max) / 2);
+    let currentElement = arr[middle];
+
+    if (currentElement < val) min = middle + 1;
+    else if (currentElement > val) max = middle - 1;
+    else return middle;
+  }
+  return -1;
+};
+
+console.log(search([1, 2, 3, 4, 5, 6, 7], 4));
